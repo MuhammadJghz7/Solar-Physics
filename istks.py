fc9c7131ac11a10ff8bdbef248c967a71f7a5ba4
MuhammadJghz7 <145560831+MuhammadJghz7@users.noreply.github.com>
2025-09-29 12:08:58 +0330

    Delete istks.py

--- istks.py
+++
@@ -1,8 +0,0 @@
-import scipy
-
-database = scipy.io.readsav('D:/istks.sav')
-print(database.keys())
-istks = database['istks']
-continuum = database['continuum']
-print(istks.shape)
-print(continuum.shape)
